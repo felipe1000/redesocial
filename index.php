c73413bc7ea140288cc8c06633dbea818d553c48
felipe1000 <felipeguedes01@hotmail.com>
2014-08-20 00:16:47 -0300

    enviando primeiros arquivos

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:::Rede Social:::</title>
+<link href="divs.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="cabecalho"><h1>Rede Social</h1></div>
+<div id="container">Content for  id "container" Goes Here</div>
+</body>
+</html>
